dfd7a11e79ba5fef92fa424a3a3fba2332d0ff46
Zxw854953678 <Zxw854953678@163.com>
2016-04-29 00:04:16 +0800

    modified funtion name

--- lcdDigits.js
+++ lcdDigits.js
@@ -1,3 +1,82 @@
-/**
- * Created by kathy on 16-4-28.
- */
+function buildDigits(digit){
+    var digitStringArray = digit.toString().split('');
+    var digitArray = [];
+
+    digitStringArray.forEach(function(digitString){
+        digitArray.push(Number(digitString));
+    });
+
+    return digitArray;
+}
+
+function loadAllDigits(){
+
+    return {
+        0:['._.','|.|','|_|'],
+        1:['...','..|','..|'],
+        2:['._.','._|','|_.'],
+        3:['._.','._|','._|'],
+        4:['...','|_|','..|'],
+        5:['._.','|_.','._|'],
+        6:['._.','|_.','|_|'],
+        7:['._.','..|','..|'],
+        8:['._.','|_|','|_|'],
+        9:['._.','|_|','..|']
+    };
+
+}
+
+function biuldLcdString(digitsArray,allDigitsString){
+    var digitsText = [];
+
+    digitsArray.forEach(function(digit){
+        digitsText.push(allDigitsString[digit]);
+    });
+
+    return digitsText;
+}
+
+function printLcdDigits(lcdStringArray){
+    var lcdDigits = '';
+    for(var i=0; i<3; i++){
+        lcdDigits += lcdDigitReceipt(lcdStringArray,i);
+    }
+
+    return lcdDigits;
+}
+
+function lcdDigitReceipt(lcdStringArray,index){
+    var digitReceipt = '';
+
+    for(var j=0; j<lcdStringArray.length; j++){
+
+        digitReceipt += lcdStringArray[j][index] ;
+        if(j === lcdStringArray.length-1)
+            digitReceipt += '\n';
+        else
+            digitReceipt += ' ';
+    }
+
+    return digitReceipt;
+}
+
+function printDigit(digit){
+    console.log("run begin...");
+
+    var digitsArray = buildDigits(digit);
+    var allDigitsString = loadAllDigits();
+    var lcdStringArray = biuldLcdString(digitsArray,allDigitsString);
+
+    var lcdDigits = printLcdDigits(lcdStringArray);
+    console.log(lcdDigits);
+}
+
+module.exports =
+{
+    loadAllDigits:loadAllDigits,
+    buildDigits:buildDigits,
+    biuldLcdString:biuldLcdString,
+    lcdDigitReceipt:lcdDigitReceipt,
+    printLcdDigits:printLcdDigits,
+    printDigit:printDigit
+};
